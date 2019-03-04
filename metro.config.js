4815c795f65b794ab883bc8c368af7b0eba70275
Grzegorz Mandziak <alimek@alimek.pl>
2019-03-04 01:01:36 +0100

    init commit

---
+++ metro.config.js
@@ -0,0 +1,18 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+// @ts-ignore
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+};
